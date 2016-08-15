5ce368b1d47da99d7faab060b9060cb971daa7c1
Hanzal Salim <hanzalsalim94@gmail.com>
2016-08-15 21:48:56 +0530

    json load error corrected

--- createdb.py
+++ createdb.py
@@ -7,7 +7,7 @@ db.init_app(app)
 from app import HotelChains, Hotels, Reviews
 with app.app_context():
 
-    with open('static/json/relation.json') as relations_file:    
+    with open('static/json/relation.json', encoding='utf8') as relations_file:    
         relations = json.load(relations_file)
         for relation in relations:
             HotelChain = HotelChains(name = relation['parent']['name'], property_id = relation['parent']['property_id'])
@@ -18,7 +18,7 @@ with app.app_context():
                 new_hotel.save()
 
 
-    with open('static/json/reviews.json') as reviews_file:    
+    with open('static/json/reviews.json', encoding='utf8') as reviews_file:    
         reviews = json.load(reviews_file)
         for review in reviews:
             hotel = Hotels.query.filter(Hotels.property_id == review['property_id']).first()
